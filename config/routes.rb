7ee8263ac13ccc84dd654e275b34c8f9022903e0
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-06-01 14:34:56 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  resources :products
-  get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :products
+  
+  resource :cart do
+    get :list_products
+    post :add_product
+    patch :update_quantity
+    delete :remove_product
+  end
+  get "up" => "rails/health#show", as: :rails_health_check
   root "rails/health#show"
 end
+
