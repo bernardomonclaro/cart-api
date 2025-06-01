a2fb1a4b69a0f7f333b45302bd6454e32ae8a481
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-06-01 14:30:46 -0300

    adds last_interaction_at and default to card total_price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_30_204456) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_01_143309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,10 +26,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_30_204456) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.decimal "total_price", precision: 17, scale: 2
+    t.decimal "total_price", precision: 17, scale: 2, default: "0.0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "abandoned_at"
+    t.datetime "last_interaction_at"
   end
 
   create_table "products", force: :cascade do |t|
