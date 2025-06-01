3046b8fb72d286cbaf072e5e2d0cbc49075b0b2b
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-06-01 14:32:12 -0300

    fix carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  def show
+  def list_products
     cart = current_cart
     render json: serialize_cart(cart)
   end
@@ -12,7 +12,8 @@ class CartsController < ApplicationController
     raise "Invalid quantity" if quantity <= 0
 
     cart_item = cart.cart_items.find_or_initialize_by(product: product)
-    cart_item.quantity += quantity
+    cart_item.quantity = (cart_item.quantity || 0) + quantity
+    cart_item.unit_price ||= product.price
     cart_item.save!
 
     cart.touch(:last_interaction_at)
@@ -21,11 +22,12 @@ class CartsController < ApplicationController
     render json: serialize_cart(cart)
   end
 
+
   def update_quantity
     cart = current_cart
-    cart_item = cart.cart_items.find_by!(product_id: params[:product_id])
-    cart_item.quantity = params[:quantity]
-    cart_item.save!
+    cart_item = cart.cart_items.find_by(product_id: params[:product_id])
+    return render json: { error: 'Product not found in cart' }, status: :not_found unless cart_item
+    cart_item.update!(quantity: params[:quantity])
 
     cart.touch(:last_interaction_at)
     cart.update_total_price!
