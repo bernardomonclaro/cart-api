2900eba4038ee7437ecdf08768e45787bd5946b6
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-06-02 02:23:11 -0300

    carts and products factory

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -1,7 +1,17 @@
 FactoryBot.define do
-  factory :shopping_cart, class: 'Cart' do
+  factory :cart, class: 'Cart' do
     total_price { 0.0 }
     last_interaction_at { Time.current }
+
+    trait :empty do
+      # nenhum cart_item
+    end
+
+    trait :with_products do
+      after(:create) do |cart|
+        create_list(:cart_item, 2, cart: cart)
+      end
+    end
 
     trait :abandoned do
       abandoned_at { 8.days.ago }
