2e3434a62810ada9cda1a510413cda00038cc060
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-06-02 01:09:17 -0300

    cart spec routing

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -2,16 +2,20 @@ require "rails_helper"
 
 RSpec.describe CartsController, type: :routing do
   describe 'routes' do
-    it 'routes to #show' do
-      expect(get: '/cart').to route_to('carts#show')
+    it 'routes to #add_product' do
+      expect(post: '/cart/add_product').to route_to('carts#add_product')
     end
 
-    it 'routes to #create' do
-      pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}" 
+    it 'routes to #list_products' do
+      expect(get: '/cart/list_products').to route_to('carts#list_products')
     end
 
-    it 'routes to #add_item via POST' do
-      expect(post: '/cart/add_item').to route_to('carts#add_item')
+    it 'routes to #update_quantity' do
+      expect(patch: '/cart/update_quantity').to route_to('carts#update_quantity')
+    end
+
+    it 'routes to #remove_product' do
+      expect(delete: '/cart/remove_product').to route_to('carts#remove_product')
     end
   end
-end 
+end
