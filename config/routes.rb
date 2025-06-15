2c439f2c9aafc89ef58ade7e6d048a649524a5a9
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-06-15 11:54:22 -0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
   resources :products
-  
-  resource :cart do
-    get :list_products
-    post :add_product
-    patch :update_quantity
-    delete :remove_product
-  end
-  get "up" => "rails/health#show", as: :rails_health_check
-  root "rails/health#show"
-end
 
+  post '/carts', to: 'carts#create'
+  post '/cart/add_item', to: 'carts#add_item'
+  get '/cart', to: 'carts#show'
+  delete '/cart/:product_id', to: 'carts#destroy'
+
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  root 'rails/health#show'
+end
