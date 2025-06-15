0cba436134a5af4416d9e635e9b8fa3c4caedabb
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-06-15 11:51:37 -0300

    job refactor

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,19 +1,11 @@
+# frozen_string_literal: true
+
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # Marks carts that have been inactive for more than 3 hours as abandoned
-    Cart.where(abandoned_at: nil)
-        .where("last_interaction_at < ?", 3.hours.ago)
-        .find_each do |cart|
-          cart.mark_as_abandoned
-        end
+  def perform
+    carts = Cart.where('last_interaction_at < ?', 3.hours.ago)
 
-    # Removes carts abandoned for more than 7 days
-    Cart.where.not(abandoned_at: nil)
-        .where("abandoned_at < ?", 7.days.ago)
-        .find_each do |cart|
-          cart.remove_if_abandoned
-        end
+    Carts::AbandonedService.call(carts)
   end
 end
