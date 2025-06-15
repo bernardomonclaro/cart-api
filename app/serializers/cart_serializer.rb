15cf118cb2c2a8036154ee1a130825b028a62399
bernardomonclaro <usuarioususario@MacBook-Pro-de-usuario.local>
2025-06-15 11:49:14 -0300

    serializers refactor

--- app/serializers/cart_serializer.rb
+++ app/serializers/cart_serializer.rb
@@ -1,21 +1,15 @@
-class CartSerializer
-  def initialize(cart)
-    @cart = cart
+# frozen_string_literal: true
+
+class CartSerializer < ActiveModel::Serializer
+  attributes :id, :products, :total_price
+
+  def products
+    object.cart_items.map do |cart_item|
+      CartItemSerializer.new(cart_item).attributes
+    end
   end
 
-  def as_json
-    {
-      id: @cart.id,
-      products: @cart.cart_items.includes(:product).map do |item|
-        {
-          id: item.product.id,
-          name: item.product.name,
-          quantity: item.quantity,
-          unit_price: item.unit_price.to_f,
-          total_price: item.total_price.to_f
-        }
-      end,
-      total_price: @cart.total_price.to_f
-    }
+  def total_price
+    products.sum { |product| product[:total_price] }
   end
 end
